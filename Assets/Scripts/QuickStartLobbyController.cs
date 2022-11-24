34cdc8111f1e1eedd4097171a9f10162dc201467
Stavrin <steffanleprince@gmail.com>
2022-11-24 11:53:49 +0000

    Working on QuickStartLobbyController script.

--- Assets/Scripts/QuickStartLobbyController.cs
+++ Assets/Scripts/QuickStartLobbyController.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using Unity.VisualScripting;
 
-public class QuickStartLobbyController : MonoBehaviour
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject StartButton;
+    private int RoomSize;
+
+    void Awake()
     {
-        
+        StartButton.onClick.AddListener(ClickStart); //makes the function ClickStart happen when StartButton is clicked.
+
     }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        StartButton.SetActive(true);
+
+    }
+
+    public void ClickStart()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Quick start")
+
+
+
+    }
+
+
 
     // Update is called once per frame
     void Update()
