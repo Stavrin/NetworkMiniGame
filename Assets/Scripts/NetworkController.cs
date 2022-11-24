926c5c6efc7902291d005e29c050d5107f6f66da
Stavrin <steffanleprince@gmail.com>
2022-11-24 11:01:03 +0000

    NetworkManager script created, Photon is plumbed in.

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+/* Documentation: https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+Scripting API: https://doc-api.photonengine.com/en/pun/v2/index.html
+*/
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        Debug.Log("We are connected to the" + PhotonNetwork.CloudRegion + " server!")
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
